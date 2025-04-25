bed6c7c8f1aa25d25992ea7a0079e699be013ac5
Iliana Genkova <iliana.genkova@noaa.gov>
2025-04-25 03:28:17 +0000

    Populated dumpmb repo with code from NCO instalation:  /apps/ops/prod/nco/intel/19.1.3.304/dumpmb.v1.0.0

---
+++ sorc/build.sh
@@ -0,0 +1,17 @@
+set -eua
+
+. ../versions/build.ver
+
+module reset
+module load PrgEnv-intel/${PrgEnv_intel_ver}
+module load intel/${intel_ver}
+module load craype/${craype_ver} 
+module load w3emc/${w3emc_ver}
+module load w3nco/${w3nco_ver}
+module load bufr/${bufr_ver}
+module list
+
+mkdir -p ../exec
+
+(cd bufr_argodump.fd; ./makefile.sh)
+(cd bufr_prepmods.fd; ./makefile.sh)
